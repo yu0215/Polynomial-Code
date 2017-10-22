94e0fe6b1849ffcd0eb8837f3974638d36700458
yu0215 <yu0215@mit.edu>
2017-10-22 17:13:39 +0800

    Decoding Verification

--- Encoding-at-Master/Master.py
+++ Encoding-at-Master/Master.py
@@ -17,9 +17,9 @@ F=65537 #field size assumed to be prime for this implementation
 
 #matrix input: A:s by r, B:s by t 
 
-s=90
-r=100
-t=120
+s=9
+r=10
+t=8
 
 #creat random matrices of 8-bit ints
 A=np.matrix(np.random.random_integers(0,255,(r,s)))
@@ -39,6 +39,9 @@ print (end - start)
 Aenc=Encoder.Enc(Ap,N,m,1,F)
 Benc=Encoder.Enc(Bp,N,n,m,F)
 
+
+end = datetime.now()
+print (end - start)
 #TODO: give Aenc[i],Benc[i] to worker i
 #time the delivery (back and forth) and computing
 
@@ -49,11 +52,27 @@ Benc=Encoder.Enc(Bp,N,n,m,F)
 lst=range(m*n)
 Crtn=[Aenc[i]*(Benc[i].getT()) for i in range(m*n)]
 
-#classical decoding algorithm
-
-#for i in range(m*n):
-    
-
 end = datetime.now()
 print (end - start)
 
+#classical decoding algorithm
+
+Jacob=[np.array([pow(lst[i],j,F) for j in range(m*n)]) for i in range(m*n)]
+
+#Forward Gaussian Elimination
+for i in range(m*n):
+    for j in range(i+1,m*n):
+        Crtn[j]=((Crtn[j]-Crtn[i])*pow(lst[j]-lst[i],F-2,F))%F
+        Jacob[j]=((Jacob[j]-Jacob[i])*pow(lst[j]-lst[i],F-2,F))%F
+#Backward Gaussian Elimination
+for i in range(m*n-1,0,-1):
+    for j in range(i):
+        Crtn[j]=(Crtn[j]-Crtn[i]*Jacob[j][i])%F
+        
+end = datetime.now()
+print (end - start)
+
+#verify correctness
+
+Cver=[(Ap[i%n]*Bp[i/n].getT())%F for i in range(m*n)]
+print ([np.array_equal(Crtn[i],Cver[i]) for i in range(m*n)])
