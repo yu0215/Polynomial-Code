c612cee366e15c9b6095850463dfd218a01bc7c6
yu0215 <yu0215@mit.edu>
2017-10-30 14:38:05 +0800

    Added Fast Decoding

--- Encoding-at-Master/Master_Decoding.py
+++ Encoding-at-Master/Master_Decoding.py
@@ -1,5 +1,8 @@
 #Computation at master node:
 
+
+#fast decoding algorithm: hardcoded for m=n=4, F=65537=2**(2**sig)+1
+
 import numpy as np
 from datetime import datetime
 import Encoder 
@@ -26,14 +29,6 @@ t=4000
 A=np.matrix(np.random.random_integers(0,255,(r,s)))
 B=np.matrix(np.random.random_integers(0,255,(t,s)))
 
-#test case 1: M=N=2
-#A=np.matrix([[87,204],[251,149]])
-#B=np.matrix([[74,249],[152,73]]) 
-
-#test case 2: M=N=2
-
-#A=np.matrix([[118,  18], [ 41, 246]])
-#B=np.matrix( [[ 53, 249], [  0, 210]])
 
 #polynomial code
 #split the matrices
@@ -46,8 +41,14 @@ Bp=np.split(B,n)
 end = datetime.now()
 print (end - start)
 
-Aenc=Encoder.Enc(Ap,N,m,1,F)
-Benc=Encoder.Enc(Bp,N,n,m,F)
+
+#fast decoding algorithm: hardcoded for m=n=4, F=65537=2**(2**sig)+1
+#8th primitive root 64
+rt=64
+var=[pow(64,i,65537) for i in range(16)]+[3,9]#18 distinct roots
+
+Aenc=[sum([Ap[j]*(i**(j*1)) for j in range(m)])%F for i in var] 
+Benc=[sum([Bp[j]*(i**(j*m)) for j in range(n)])%F for i in var] 
 
 
 end = datetime.now()
@@ -61,32 +62,23 @@ print (end - start)
 # example
 lst=random.sample(range(N), m*n)
 
-#test case 1: M=N=2
-#lst=[10, 7, 16, 9]
-
-#test case 2: M=N=2
-#lst= [14, 13, 5, 8]
 
 Crtn=[(Aenc[i]*(Benc[i].getT()))%F for i in lst]
 
 end = datetime.now()
 print (end - start)
 
-#numpy takes 32 bit by default, eg:
-#%x=155
-#print ((-13876738)*x)
-#print (np.matrix([[-13876738]]))*x
-#print (np.matrix([[-2**40]]))*2
+#fast decoding algorithm: hardcoded for m=n=4, F=65537=2**(2**sig)+1
+sig=4
 
-#classical decoding algorithm
+Jacob=[np.array([pow(var[lst[i]],j,F) for j in range(m*n)]) for i in range(m*n)]
 
-Jacob=[np.array([pow(lst[i],j,F) for j in range(m*n)]) for i in range(m*n)]
 
 #Forward Gaussian Elimination
 for i in range(m*n):
     for j in range(i+1,m*n):
-        Crtn[j]=((Crtn[j]-Crtn[i])*pow(lst[j]-lst[i],F-2,F))%F
-        Jacob[j]=((Jacob[j]-Jacob[i])*pow(lst[j]-lst[i],F-2,F))%F
+        Crtn[j]=((Crtn[j]-Crtn[i])*pow(var[lst[j]]-var[lst[i]],F-2,F))%F
+        Jacob[j]=((Jacob[j]-Jacob[i])*pow(var[lst[j]]-var[lst[i]],F-2,F))%F
 #Backward Gaussian Elimination
 for i in range(m*n-1,0,-1):
     for j in range(i):
