9052807b5fa5c11e295929de079e02a0d5cf6ab8
yu0215 <yu0215@mit.edu>
2017-12-28 15:16:05 -0800

    Cleaned code

--- EC2-Test/Readme.txt
+++
@@ -1,5 +0,0 @@
-Test and timing of a matrix multiplication job by a single computing node in EC2
-
-Folders:
-	
-	Source
--- Encoding-at-Master/Encoder.py
+++
@@ -1,9 +0,0 @@
-#Encoder at master mode:
-
-import numpy as np
-
-#input Mp,N
-def Enc(Mp,N,div,k,F):
-    return [sum([Mp[j]*(i**(j*k)) for j in range(div)])%F for i in range(N)]
-
-#Note use a finite field of size 65537
